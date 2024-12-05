94e2b548fd7ac7f5b33c071ee2354518a4d9d78c
sabiliwafa29 <rosyadwafa@met.student.pens.ac.id>
2024-12-05 19:41:29 +0700

    first

---
+++ server.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const tf = require('@tensorflow/tfjs-node'); // Add TensorFlow.js for Node.js
+const fs = require('fs');
+const crypto = require('crypto');
+const { Firestore } = require('@google-cloud/firestore');
+const firebase = require('firebase-admin');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let model;
+
+// Load the model
+async function loadModel() {
+  model = await tf.loadGraphModel('file://models/model.json');
+  console.log('Model loaded successfully');
+}
+
+// Call loadModel when the server starts
+loadModel();
+
+const serviceAccount = require('/home/MLGCAPI/submissionmlgc-rosyad-ee74f442da43.json'); 
+firebase.initializeApp({ 
+  credential: firebase.credential.cert(serviceAccount)
+});
+
+async function storeData(id, data) { 
+  const db = firebase.firestore();
+  const predictCollection = db.collection('predictions'); 
+  return predictCollection.doc(id).set(data);
+}
+
+// Set up storage and file size limit
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // 1MB size limit
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb(new Error('Only images are allowed'));
+    }
+  }
+}).single('image');
+
+app.post('/predict', (req, res) => {
+  upload(req, res, async (err) => {
+    if (err instanceof multer.MulterError) {
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(413).send({
+          status: 'fail',
+          message: 'Payload content length greater than maximum allowed: 1000000'
+        });
+      }
+      return res.status(400).send({
+        status: 'fail',
+        message: 'Terjadi kesalahan dalam melakukan prediksi limit'
+      });
+    } else if (err) {
+      return res.status(400).send({
+        status: 'fail',
+        message: 'Terjadi kesalahan dalam melakukan prediksi'
+      });
+    }
+    if (!req.file) {
+      return res.status(400).send({
+        status: 'fail',
+        message: 'No file uploaded'
+      });
+    }
+    
+    try {
+      const predictionResult = await predictImage(req.file.path);
+      if (predictionResult.error) {
+        throw new Error(predictionResult.error);
+      }
+      await storeData(predictionResult.data.id, predictionResult.data);
+      res.send({
+        status: 'success',
+        message: 'Model is predicted successfully',
+        data: predictionResult.data
+      });
+    } catch (predictionError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: `Terjadi kesalahan dalam melakukan prediksi ${predictionError.message}`
+      });
+    }
+  });
+});
+
+// Function to preprocess the image and make a prediction
+async function predictImage(filePath) {
+  const image = fs.readFileSync(filePath);
+  const inputTensor = tf.node
+            .decodeJpeg(image)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat()
+
+  const predictions = model.predict(inputTensor);
+  const score = await predictions.data();
+  const confidenceScore = Math.max(...score) * 100;
+  const idP = crypto.randomUUID();
+ 
+  const label = confidenceScore <= 50 ? 'Non-cancer' : 'Cancer';
+
+  if (label === 'Cancer') {
+    return {
+      data: {
+        id: idP,
+        result: "Cancer",
+        suggestion: "Segera periksa ke dokter!",
+        createdAt: new Date().toISOString()
+      }
+    };
+  } 
+  
+  if (label === 'Non-cancer') {
+    return {
+      data: {
+        id: idP,
+        result: "Non-cancer",
+        suggestion: "Penyakit kanker tidak terdeteksi.",
+        createdAt: new Date().toISOString()
+      }
+    };
+  }
+}
+
+app.get('/predict/histories', async (req, res) => { 
+  try { 
+    const db = firebase.firestore(); 
+    const predictCollection = db.collection('predictions'); 
+    const snapshot = await predictCollection.get(); 
+    const histories = snapshot.docs.map(doc => doc.data()); 
+    
+    res.send({ 
+      status: 'success', 
+      data: histories }); 
+    } catch (error) { 
+      res.status(500).send({ 
+        status: 'fail', 
+        message: `Terjadi kesalahan dalam mendapatkan histori ${error.message}` 
+      }); 
+    } 
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
